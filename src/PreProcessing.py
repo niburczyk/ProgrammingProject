e715d4e2df548a9386f71425b28e2f722af77d41
niburczyk <niklas.burczyk001@stud.fh-dortmund.de>
2024-06-21 12:21:46 +0200

    Renaming and adding plotting

--- src/PreProcessing.py
+++ src/PreProcessing.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 from scipy.io import loadmat
 from scipy.signal import butter, filtfilt
+import matplotlib.pyplot as plt
 
 # Funktion zum Einlesen der .mat-Datei
 def read_mat_file(filename):
@@ -9,7 +10,7 @@ def read_mat_file(filename):
     return data
 
 # Funktion zum Anwenden eines Bandpassfilters
-def bandpass_filter(data, lowcut, highcut, fs, order=5):
+def bandpass_filter(data, lowcut, highcut, fs, order):
     nyquist = 0.5 * fs
     low = lowcut / nyquist
     high = highcut / nyquist
@@ -21,14 +22,30 @@ def bandpass_filter(data, lowcut, highcut, fs, order=5):
 def calculate_mean(filtered_data):
     return np.mean(filtered_data, axis=0)
 
+# Funktion zum Darstellen des Signals
+def plot_signal(original_data, filtered_data, title):
+    plt.figure(figsize=(14, 7))
+    plt.subplot(2, 1, 1)
+    plt.plot(original_data, label='Original Signal')
+    plt.title(f'{title} - Original Signal')
+    plt.legend()
+
+    plt.subplot(2, 1, 2)
+    plt.plot(filtered_data, label='Filtered Signal', color='orange')
+    plt.title(f'{title} - Filtered Signal')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
+
 # Hauptskript
 if __name__ == "__main__":
     # Parameter
-    folder_path = './sample/Condition_P'  # Pfad zum Ordner mit den .mat-Dateien
+    folder_path = '.\sample\Condition_F'  # Pfad zum Ordner mit den .mat-Dateien
     lowcut = 0.5  # untere Grenzfrequenz des Bandpassfilters
-    highcut = 30.0  # obere Grenzfrequenz des Bandpassfilters
+    highcut = 10.0  # obere Grenzfrequenz des Bandpassfilters
     fs = 2000  # Abtastfrequenz in Hz
-    order = 5  # Filterordnung
+    order = 1  # Filterordnung
 
     # Durch alle Dateien im Ordner iterieren
     for filename in os.listdir(folder_path):
@@ -47,9 +64,12 @@ if __name__ == "__main__":
             # Berechnen des Mittelwerts für jeden Vektor
             mean_vector = calculate_mean(filtered_data)
 
-            # Den Mittelwertvektor speichern oder weiter verarbeiten
+            # Optional: Den Mittelwertvektor speichern oder weiter verarbeiten
             print(f"Mean Vector for {filename}:", mean_vector)
 
-            # Den Mittelwertvektor in eine Datei speichern
+            # Optional: Den Mittelwertvektor in eine Datei speichern
             output_filename = f'mean_vector_{os.path.splitext(filename)[0]}.npy'
-            np.save(os.path.join(folder_path, output_filename), mean_vector)
+            #np.save(os.path.join(folder_path, output_filename), mean_vector)
+
+            # Darstellen des Original- und des gefilterten Signals
+            plot_signal(data, filtered_data, os.path.splitext(filename)[0])
