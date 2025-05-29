721f07fa5766779fe5d7f772ac6bb9df8fe8af21
niburczyk <166651542+niburczyk@users.noreply.github.com>
2025-05-29 17:56:49 +0200

    fix path

--- scripts/flash_arduino.sh
+++ scripts/flash_arduino.sh
@@ -3,7 +3,7 @@ set -e
 
 PORT=/dev/ttyACM0
 BOARD=arduino:avr:uno
-SKETCH=src/svm_predict_poly
+SKETCH=src/svm_predict_poly/emg_transmitter.ino
 
 echo "🔌 Arduino flashen..."
 arduino-cli compile --fqbn $BOARD $SKETCH
